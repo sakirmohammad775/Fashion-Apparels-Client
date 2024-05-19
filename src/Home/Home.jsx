38d3d0e6c36d28468f1e1550e7f4f2c090d71ee6
Sakir Mohammad <sakirmohammad775@gmail.com>
2024-05-19 21:58:59 +0600

    start with navbar and footer

---
+++ src/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+       <>
+       <Navbar></Navbar>
+       <Footer></Footer>
+       </>
+    );
+};
+
+export default Home;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Home/Home";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Home></Home>
     },
   ]);
