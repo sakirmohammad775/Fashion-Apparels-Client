719da926ab6878f5976d4fd90b81f2878baf39d3
Sakir Mohammad <sakirmohammad775@gmail.com>
2024-05-19 23:46:22 +0600

    banner added

---
+++ src/Home/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Footer from "../../Shared/Footer/Footer";
+import Navbar from "../../Shared/Navbar/Navbar";
+import Banner from "../Banner";
+
+
+
+const Home = () => {
+    return (
+       <>
+       <Navbar></Navbar>
+       <Banner></Banner>
+       <Footer></Footer>
+       </>
+    );
+};
+
+export default Home;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../Home/Home";
+import Home from "../Home/Home/Home";
+
+
 
 export const router = createBrowserRouter([
     {
