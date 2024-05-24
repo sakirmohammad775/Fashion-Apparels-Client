0281e400e02e99eff5406a2f1e7e93bca98eb0e8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-05-24 23:45:01 +0600

    firebase added

--- src/SIgnUp/SignUp.jsx
+++ src/SIgnUp/SignUp.jsx
@@ -1,10 +1,10 @@
-
+import { FaFacebook, FaGoogle } from "react-icons/fa6";
 const SignUp = () => {
     return (
         <>
         
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="hero-content flex-col ">
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">SignUp</h1>
                         <p className="py-6">Explore Our page</p>
@@ -13,15 +13,15 @@ const SignUp = () => {
                         <form className="card-body">
                         <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text">Name</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="name" placeholder="name" name="name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
@@ -32,8 +32,13 @@ const SignUp = () => {
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                            <div className="form-control mt-3">
+                                <button className="btn btn-primary">SignUp</button>
+                            </div>
+                            <div className="flex my-5 justify-center gap-6">
+                                
+                                <button className="pt-2"><FaGoogle /></button>
+                                <button className="pt-2"><FaFacebook /></button>
                             </div>
                         </form>
                     </div>
