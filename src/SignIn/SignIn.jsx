fd17bbc50e848c668bbc5cc371bc9639a839f703
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-05-26 22:49:08 +0600

    signUp page setup and images added

--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -1,35 +1,65 @@
 
-
+import { FaFacebook, FaGoogle } from "react-icons/fa6";
+import signInImage from "../assets/login2.jpg"
+import { Link } from "react-router-dom";
 const SignIn = () => {
+    const backgroundImageStyle = {
+        backgroundImage: `url(${signInImage})`,
+        backgroundSize: 'cover', // Cover the entire div
+        backgroundPosition: 'center', // Center the image
+        backgroundRepeat: 'no-repeat', // Do not repeat the image
+    };
     return (
         <>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="hero min-h-screen" style={backgroundImageStyle}>
+                <div className="hero-content flex-col ">
                     <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <h1 className="text-5xl font-bold">SignUp</h1>
+                        <p className="py-6">Explore Our page</p>
                     </div>
-                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card  w-full max-w-lg bg-transparent">
                         <form className="card-body">
-                            <div className="form-control">
+                            <div className="form-control w-96">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="font-bold">Name</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="name" placeholder="name" name="name" className="input input-bordered bg-transparent  " required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="font-bold">Email</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="email" placeholder="email"
+                                    name="email"
+                                    className="input input-bordered bg-transparent" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="font-bold">Password</span>
+                                </label>
+                                <input type="password"
+                                    name="password"
+                                    placeholder="password"
+                                     className="input input-bordered bg-transparent" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                            <div className="form-control mt-3">
+                                <button type="submit" className="btn  bg-transparent">SignUp</button>
                             </div>
                         </form>
+                        {/*if error found*/}
+                       
+
+                        <p className="text-center">Already have an account? <Link to="/signIn">Sign In</Link></p>
+
+                        {/*another login process*/}
+                        <div className="flex my-5 justify-center gap-6">
+
+                            <button className="pt-2"><FaGoogle /></button>
+                            <button className="pt-2"><FaFacebook/></button>
+                        </div>
                     </div>
                 </div>
             </div>
