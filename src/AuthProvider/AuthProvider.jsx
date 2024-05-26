361149520d5985038b5c9cf63bac13ca0c900551
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-05-26 21:39:55 +0600

    the user state is updated with the current user

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,45 @@
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { app } from "../Firebase/Firebase.config";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext()
+
+const auth = getAuth(app)
+const AuthProvider = () => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const signIn=(email,password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    const logOut=()=>{
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    // When the authentication state changes, the user state is updated with the current user, and the loading state is set to false.
+    useEffect(()=>{
+        const unSubscribe=onAuthStateChanged(auth,currentUser=>{
+            setUser(currentUser)
+            console.log('current User:',currentUser);
+            setLoading(true)
+        })
+        return()=>{
+            return unSubscribe  // Clean up the subscription
+        }
+    })
+
+
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default AuthProvider;
--- src/SIgnUp/SignUp.jsx
+++ src/SIgnUp/SignUp.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { FaFacebook, FaGoogle } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 const SignUp = () => {
-    const [isSignUp, setSignUp] = useState(true)
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [error, setError] = useState('')
@@ -20,7 +21,7 @@ const SignUp = () => {
                         <p className="py-6">Explore Our page</p>
                     </div>
                     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body">
+                        <form onSubmit={handleSubmit} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
@@ -31,26 +32,40 @@ const SignUp = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                                <input type="email" placeholder="email" 
+                                name="email" 
+                                value={email}
+                                onChange={(e)=>setEmail(e.email.value)} 
+                                className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" 
+                                name="password"
+                                value={password}
+                                onChange={(e)=>setPassword(e.target.value)}
+                                placeholder="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-3">
-                                <button className="btn btn-primary">SignUp</button>
-                            </div>
-                            <div className="flex my-5 justify-center gap-6">
-
-                                <button className="pt-2"><FaGoogle /></button>
-                                <button className="pt-2"><FaFacebook /></button>
+                                <button type="submit" className="btn btn-primary">SignUp</button>
                             </div>
                         </form>
+                        {/*if error found*/}
+                        {error && <p style={{ color: 'red' }}>{error}</p>}
+
+                        <p>Already have an account? <Link to="/signIn">Sign In</Link></p>
+
+                        {/*another login process*/}
+                        <div className="flex my-5 justify-center gap-6">
+
+                            <button className="pt-2"><FaGoogle /></button>
+                            <button className="pt-2"><FaFacebook /></button>
+                        </div>
                     </div>
                 </div>
             </div>
